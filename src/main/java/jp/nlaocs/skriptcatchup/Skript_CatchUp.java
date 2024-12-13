a7e4748c665baf5dd4ed082ba30d191b8e3b3e95
あお <113911070+nlaocs@users.noreply.github.com>
2024-12-14 02:38:31 +0900

    gradle init

---
+++ src/main/java/jp/nlaocs/skriptcatchup/Skript_CatchUp.java
@@ -0,0 +1,17 @@
+package jp.nlaocs.skriptcatchup;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Skript_CatchUp extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
