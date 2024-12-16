5347dce5c0fb09191b47e06b0bef1a56024b776c
あお <113911070+nlaocs@users.noreply.github.com>
2024-12-17 00:33:28 +0900

    SkriptにAddonとして登録させる

--- src/main/java/jp/nlaocs/skriptcatchup/Skript_CatchUp.java
+++ src/main/java/jp/nlaocs/skriptcatchup/Skript_CatchUp.java
@@ -2,16 +2,33 @@ package jp.nlaocs.skriptcatchup;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import ch.njol.skript.Skript;
+import ch.njol.skript.SkriptAddon;
+
 public final class Skript_CatchUp extends JavaPlugin {
+
+    static Skript_CatchUp instance;
+    SkriptAddon addon;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
+        addon = Skript.registerAddon(this);
+
+        try {
+            addon.loadClasses("jp.nlaocs.skriptcatchup", "elements");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static Skript_CatchUp getInstance() {
+        return instance;
     }
 }
